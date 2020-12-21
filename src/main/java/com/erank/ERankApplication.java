1d6672bd791bc5bbc4b540f7fc316bc5c194f073
AjayAbhi <ajaymandagiri400@gmail.com>
2020-12-21 18:22:24 +0530

    Inital Commit

---
+++ src/main/java/com/erank/ERankApplication.java
@@ -0,0 +1,13 @@
+package com.erank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERankApplication.class, args);
+	}
+
+}
